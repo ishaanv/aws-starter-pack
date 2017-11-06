fc9100ce6d7dedc3f6535d4745a1e606c3b0c5a3
Ishaan Varshney <ishaan.varshney@gmail.com>
2017-11-06 12:14:53 +1100

    :balloon: initial commit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+# install oh-my-zsh
+apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# set to default editor
+sudo chsh -s /usr/bin/zsh ubuntu
+
